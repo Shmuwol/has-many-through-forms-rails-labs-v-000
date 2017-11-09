ad36225a36fef8e9519ba337fecf7a4aced6e419
Shmully Wolfson <shmullywolfson@gmail.com>
2017-11-09 14:12:32 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,21 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
-  def categories_attributes=(categories_hashes)
-    categories_hashes.each do |i, categories_attributes|
-      category = Category.find_or_create_by(name: categories_attributes[:name])
-      self.post_categories.build(category: category)
-    end
-  end
 
+  # def categories_attributes=(categories_hashes)
+  #
+  #   categories_hashes.each do |i, categories_attributes|
+  #     if categories_attributes[:name].present?
+  #       category = Category.find_or_create_by(name: categories_attributes[:name])
+  #       if !self.categories.include?(category)
+  #         self.post_categories.build(category: category)
+  #       end
+  #     end
+  #   end
+  #
+  # end
 
 
 end
